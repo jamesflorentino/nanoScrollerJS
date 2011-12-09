jamesflorentino/nanoScrollerJS

commit 34a6e7fbc55dcd80c0f91bc70224bbd14a9ff787

Merged fork from Diom. Also renamed a few properties

bin/javascripts/jquery.nanoscroller.js
@@ -1,88 +1,87 @@
 (function() {
   var $, NanoScroll;
-
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
   $ = this.jQuery;
-
   NanoScroll = (function() {
-
-    function NanoScroll(target, options) {
-      this.slider = null;
-      this.pane = null;
-      this.content = null;
-      this.scrollH = 0;
-      this.sliderH = 0;
-      this.paneH = 0;
-      this.sliderY = 0;
-      this.offsetY = 0;
-      this.contentH = 0;
-      this.contentY = 0;
-      this.isDrag = false;
-      options || (options = {});
-      this.target = target;
+    function NanoScroll(el, options) {
+      /* Probably not necessarry to define this for filesize reasons
+      @slider   = null
+      @pane     = null
+      @content  = null
+      @scrollH  = 0
+      @sliderH  = 0
+      @paneH    = 0
+      @sliderY  = 0
+      @offsetY  = 0
+      @contentH = 0
+      @contentY = 0
+      @isDrag   = false
+      */      options || (options = {});
+      this.el = target;
       this.generate();
       this.createEvents();
-      this.assignEvents();
+      this.addEvents();
       this.reset();
       return;
     }
-
     NanoScroll.prototype.createEvents = function() {
-      var _this = this;
-      return this.handler = {
-        down: function(e) {
-          _this.isDrag = true;
-          _this.offsetY = e.clientY - _this.slider.offset().top;
-          _this.pane.addClass('active');
-          $(document).bind('mousemove', _this.handler.drag);
-          $(document).bind('mouseup', _this.handler.up);
+      var mousemove, mouseup;
+      mousemove = 'mousemove';
+      mouseup = 'mouseup';
+      return this.events = {
+        down: __bind(function(e) {
+          this.isDrag = true;
+          this.offsetY = e.clientY - this.slider.offset().top;
+          this.pane.addClass('active');
+          $(document).bind(mousemove, this.events.drag);
+          $(document).bind(mouseup, this.events.up);
           return false;
-        },
-        drag: function(e) {
-          _this.sliderY = e.clientY - _this.target.offset().top - _this.offsetY;
-          _this.scroll();
+        }, this),
+        drag: __bind(function(e) {
+          this.sliderY = e.clientY - this.el.offset().top - this.offsetY;
+          this.scroll();
           return false;
-        },
-        up: function(e) {
-          _this.isDrag = false;
-          _this.pane.removeClass('active');
-          $(document).unbind('mousemove', _this.handler.drag);
-          $(document).unbind('mouseup', _this.handler.up);
+        }, this),
+        up: __bind(function(e) {
+          this.isDrag = false;
+          this.pane.removeClass('active');
+          $(document).unbind(mousemove, this.events.drag);
+          $(document).unbind(mouseup, this.events.up);
           return false;
-        },
-        resize: function(e) {
-          _this.reset();
-          return _this.scroll();
-        },
-        panedown: function(e) {
-          _this.sliderY = e.clientY - _this.target.offset().top - _this.sliderH * .5;
-          _this.scroll();
-          return _this.handler.down(e);
-        },
-        scroll: function(e) {
+        }, this),
+        resize: __bind(function(e) {
+          this.reset();
+          return this.scroll();
+        }, this),
+        panedown: __bind(function(e) {
+          this.sliderY = e.clientY - this.el.offset().top - this.sliderH * .5;
+          this.scroll();
+          return this.events.down(e);
+        }, this),
+        scroll: __bind(function(e) {
           var top;
-          if (_this.isDrag === true) return;
-          top = _this.content[0].scrollTop / _this.content[0].scrollHeight * (_this.paneH + 5);
-          return _this.slider.css({
+          if (this.isDrag === true) {
+            return;
+          }
+          top = this.content[0].scrollTop / this.content[0].scrollHeight * (this.paneH + 5);
+          return this.slider.css({
             top: Math.floor(top)
           });
-        }
+        }, this)
       };
     };
-
-    NanoScroll.prototype.assignEvents = function() {
-      $(window).bind('resize', this.handler.resize);
-      this.slider.bind('mousedown', this.handler.down);
-      this.pane.bind('mousedown', this.handler.panedown);
-      return this.content.bind('scroll', this.handler.scroll);
+    NanoScroll.prototype.addEvents = function() {
+      $(window).bind('resize', this.events.resize);
+      this.slider.bind('mousedown', this.events.down);
+      this.pane.bind('mousedown', this.events.panedown);
+      return this.content.bind('scroll', this.events.scroll);
     };
-
-    NanoScroll.prototype.removeEventListeners = function() {
-      $(window).unbind('resize', this.handler.resize);
-      this.slider.unbind('mousedown', this.handler.down);
-      this.pane.unbind('mousedown', this.handler.panedown);
-      return this.content.unbind('scroll', this.handler.scroll);
+    NanoScroll.prototype.removeEvents = function() {
+      $(window).unbind('resize', this.events.resize);
+      this.slider.unbind('mousedown', this.events.down);
+      this.pane.unbind('mousedown', this.events.panedown);
+      return this.content.unbind('scroll', this.events.scroll);
     };
-
     NanoScroll.prototype.getScrollbarWidth = function() {
       var noscrollWidth, outer, yesscrollWidth;
       outer = document.createElement('div');
@@ -96,27 +95,29 @@
       document.body.removeChild(outer);
       return noscrollWidth - yesscrollWidth;
     };
-
     NanoScroll.prototype.generate = function() {
-      this.target.append('<div class="pane"><div class="slider"></div></div>');
-      this.content = $(this.target.children()[0]);
-      this.slider = this.target.find('.slider');
-      this.pane = this.target.find('.pane');
+      this.el.append('<div class="pane"><div class="slider"></div></div>');
+      this.content = $(this.el.children()[0]);
+      this.slider = this.el.find('.slider');
+      this.pane = this.el.find('.pane');
       this.scrollW = this.getScrollbarWidth();
-      if (this.scrollbarWidth === 0) this.scrollW = 0;
+      if (this.scrollbarWidth === 0) {
+        this.scrollW = 0;
+      }
       this.content.css({
         right: -this.scrollW + 'px'
       });
       if ($.browser.msie != null) {
-        if (parseInt($.browser.version) < 8) this.pane.hide();
+        if (parseInt($.browser.version) < 8) {
+          this.pane.hide();
+        }
       }
     };
-
     NanoScroll.prototype.reset = function() {
       if (this.isDead === true) {
         this.isDead = false;
         this.pane.show();
-        this.assignEvents();
+        this.addEvents();
       }
       this.contentH = this.content[0].scrollHeight;
       this.paneH = this.pane.height();
@@ -124,11 +125,14 @@
       this.scrollH = this.paneH - this.sliderH;
       this.slider.height(this.sliderH);
     };
-
     NanoScroll.prototype.scroll = function() {
       var scrollValue;
-      if (this.sliderY < 0) this.sliderY = 0;
-      if (this.sliderY > this.scrollH) this.sliderY = this.scrollH;
+      if (this.sliderY < 0) {
+        this.sliderY = 0;
+      }
+      if (this.sliderY > this.scrollH) {
+        this.sliderY = this.scrollH;
+      }
       scrollValue = this.paneH - this.contentH + this.scrollW;
       scrollValue = scrollValue * this.sliderY / this.scrollH;
       this.content.scrollTop(-scrollValue);
@@ -136,29 +140,23 @@
         top: this.sliderY
       });
     };
-
     NanoScroll.prototype.scrollBottom = function(offsetY) {
       this.reset();
       this.sliderY = this.scrollH;
       this.scroll();
     };
-
     NanoScroll.prototype.scrollTop = function(offsetY) {
       this.reset();
       this.sliderY = 0;
       this.scroll();
     };
-
     NanoScroll.prototype.stop = function() {
       this.isDead = true;
-      this.removeEventListeners();
+      this.removeEvents();
       this.pane.hide();
     };
-
     return NanoScroll;
-
   })();
-
   $.fn.nanoScroller = function(options) {
     var scrollbar;
     options || (options = {});
@@ -170,10 +168,15 @@
       });
       return;
     }
-    if (options.scroll === 'bottom') return scrollbar.scrollBottom();
-    if (options.scroll === 'top') return scrollbar.scrollTop();
-    if (options.stop === true) return scrollbar.stop();
+    if (options.scroll === 'bottom') {
+      return scrollbar.scrollBottom();
+    }
+    if (options.scroll === 'top') {
+      return scrollbar.scrollTop();
+    }
+    if (options.stop === true) {
+      return scrollbar.stop();
+    }
     return scrollbar.reset();
   };
-
 }).call(this);

coffeescripts/jquery.nanoscroller.coffee
@@ -2,8 +2,8 @@ $ = @jQuery
 
 class NanoScroll
 
-  constructor: (target, options) ->
-
+  constructor: (el, options) ->
+    ### Probably not necessarry to define this for filesize reasons
     @slider   = null
     @pane     = null
     @content  = null
@@ -15,36 +15,39 @@ class NanoScroll
     @contentH = 0
     @contentY = 0
     @isDrag   = false
+    ###
 
     options or= {}
-    @target   = target
-
+    @el   = target
     @generate()
     @createEvents()
-    @assignEvents()
+    @addEvents()
     @reset()
     return
   
   createEvents: ->
-    @handler =
+    ## filesize reasons
+    mousemove = 'mousemove'
+    mouseup   = 'mouseup'
+    @events =
       down: (e) =>
         @isDrag  = true
         @offsetY = e.clientY - @slider.offset().top
         @pane.addClass 'active'
-        $(document).bind 'mousemove', @handler.drag
-        $(document).bind 'mouseup', 	@handler.up
+        $(document).bind mousemove, @events.drag
+        $(document).bind mouseup, 	@events.up
         false
 
       drag: (e) =>
-        @sliderY = e.clientY - @target.offset().top - @offsetY
+        @sliderY = e.clientY - @el.offset().top - @offsetY
         @scroll()
         false
 
       up: (e) =>
         @isDrag = false
         @pane.removeClass 'active'
-        $(document).unbind 'mousemove', @handler.drag
-        $(document).unbind 'mouseup', 	@handler.up
+        $(document).unbind mousemove, @events.drag
+        $(document).unbind mouseup, 	@events.up
         false
 
       resize: (e) =>
@@ -52,9 +55,9 @@ class NanoScroll
         @scroll()
 
       panedown: (e) =>
-        @sliderY = e.clientY - @target.offset().top - @sliderH * .5
+        @sliderY = e.clientY - @el.offset().top - @sliderH * .5
         @scroll()
-        @handler.down e
+        @events.down e
 
       scroll: (e) =>
         return if @isDrag is true
@@ -62,17 +65,17 @@ class NanoScroll
         @slider.css
           top: Math.floor top
 
-  assignEvents: ->
-    $(window).bind 'resize'  , @handler.resize
-    @slider.bind 'mousedown' , @handler.down
-    @pane.bind 'mousedown'   , @handler.panedown
-    @content.bind 'scroll'   , @handler.scroll
+  addEvents: ->
+    $(window).bind 'resize'  , @events.resize
+    @slider.bind 'mousedown' , @events.down
+    @pane.bind 'mousedown'   , @events.panedown
+    @content.bind 'scroll'   , @events.scroll
   
-  removeEventListeners: ->
-    $(window).unbind 'resize'  , @handler.resize
-    @slider.unbind 'mousedown' , @handler.down
-    @pane.unbind 'mousedown'   , @handler.panedown
-    @content.unbind 'scroll'   , @handler.scroll
+  removeEvents: ->
+    $(window).unbind 'resize'  , @events.resize
+    @slider.unbind 'mousedown' , @events.down
+    @pane.unbind 'mousedown'   , @events.panedown
+    @content.unbind 'scroll'   , @events.scroll
 
   getScrollbarWidth: ->
     outer                = document.createElement 'div'
@@ -88,10 +91,10 @@ class NanoScroll
 
     
   generate: ->
-    @target.append '<div class="pane"><div class="slider"></div></div>'
-    @content = $ @target.children()[0]
-    @slider  = @target.find '.slider'
-    @pane    = @target.find '.pane'
+    @el.append '<div class="pane"><div class="slider"></div></div>'
+    @content = $ @el.children()[0]
+    @slider  = @el.find '.slider'
+    @pane    = @el.find '.pane'
     @scrollW = @getScrollbarWidth()
     @scrollW = 0 if @scrollbarWidth is 0
     @content.css
@@ -106,7 +109,7 @@ class NanoScroll
     if @isDead is true
       @isDead = false
       @pane.show()
-      @assignEvents()
+      @addEvents()
 
     @contentH  = @content[0].scrollHeight
     @paneH     = @pane.height()
@@ -137,7 +140,7 @@ class NanoScroll
 
   stop: ->
     @isDead = true
-    @removeEventListeners()
+    @removeEvents()
     @pane.hide()
     return
 

bin/index.html
@@ -10,7 +10,7 @@
   <link href='http://fonts.googleapis.com/css?family=Maven+Pro' rel='stylesheet' type='text/css'>
   <link href='http://fonts.googleapis.com/css?family=Anonymous+Pro' rel='stylesheet' type='text/css'>
   <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-  <script type="text/javascript" src="javascripts/jquery.nanoscroller.js"></script>
+  <script type="text/javascript" src="javascripts/jquery.nanoscroller.min.js"></script>
   <script type="text/javascript" src="javascripts/main.js"></script>
 
   <h1>nanoScroller.js</h1>

commit e4b5781e3b1f0674aec4ac718144dd8e3e4c2e3e

Fix constructor argument naming
Regression from 34a6e7fbc5

bin/javascripts/jquery.nanoscroller.js
@@ -4,6 +4,7 @@
   $ = this.jQuery;
   NanoScroll = (function() {
     function NanoScroll(el, options) {
+      this.el = el;
       /* Probably not necessarry to define this for filesize reasons
       @slider   = null
       @pane     = null
@@ -16,8 +17,8 @@
       @contentH = 0
       @contentY = 0
       @isDrag   = false
-      */      options || (options = {});
-      this.el = target;
+      */
+      options || (options = {});
       this.generate();
       this.createEvents();
       this.addEvents();

coffeescripts/jquery.nanoscroller.coffee
@@ -2,7 +2,7 @@ $ = @jQuery
 
 class NanoScroll
 
-  constructor: (el, options) ->
+  constructor: (@el, options) ->
     ### Probably not necessarry to define this for filesize reasons
     @slider   = null
     @pane     = null
@@ -18,7 +18,6 @@ class NanoScroll
     ###
 
     options or= {}
-    @el   = target
     @generate()
     @createEvents()
     @addEvents()