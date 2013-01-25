3cabc8aac03bc4a558a93ff67cf4b9179269e674
James Florentino <jamesflorentino@gmail.com>
2013-01-25 22:48:15 +0800

    remove publish.sh

--- publish.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p ~/Desktop/jquery.nanoscroller.$1
-cp bin/javascripts/jquery.nanoscroller.min.js ~/Desktop/jquery.nanoscroller.$1
-cp bin/css/nanoscroller.css ~/Desktop/jquery.nanoscroller.$1
-zip ~/Desktop/Archive.zip ~/Desktop/jquery.nanoscroller.$1/*
-mv ~/Desktop/Archive.zip ~/Desktop/jquery.nanoscroller.$1.zip
